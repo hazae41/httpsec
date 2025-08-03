0a5724f1f07a4deb215677a027b108f2a123d22c
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-08-03 16:28:56 +0700

    d

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,89 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export type Explicit = [{ method: string, params: any[] }, Transferable[]]
 
+export function splitAndJoin(text: string, separator: string): [string, string] {
+  const [first, ...rest] = text.split(separator)
+
+  const join = rest.join(separator)
+
+  return [first, join]
+}
+
+export function useHash() {
+  const [hash, setHash] = useState(location.hash)
+
+  const onHashChange = useCallback(() => {
+    setHash(location.hash)
+  }, [])
+
+  useEffect(() => {
+    addEventListener("hashchange", onHashChange)
+    return () => removeEventListener("hashchange", onHashChange)
+  }, [onHashChange])
+
+  return hash
+}
+
 export default function Home() {
+  const hash = useHash()
+
+  const [integrity, href] = splitAndJoin(hash.slice(1), "@")
+
+  const [hidden, setHidden] = useState(true)
+
+  const [policy, setPolicy] = useState(`script-src '${integrity}';`)
+
+  useEffect(() => {
+    setPolicy(`script-src '${integrity}';`)
+  }, [integrity])
+
   const iframe = useRef<HTMLIFrameElement>(null)
 
-  const [integrity, ...href] = location.hash.slice(1).split("@")
-
-  const policy = useRef(`script-src '${integrity}';`)
-
-  const [shown, setShown] = useState(false)
-
-  const [key, setKey] = useState(crypto.randomUUID())
-
-  const onMessage = useCallback((event: MessageEvent<Explicit>) => {
+  const routeOrThrow = useCallback((event: MessageEvent<Explicit>) => {
     const [request] = event.data
 
     if (request.method === "csp_get") {
-      iframe.current?.contentWindow?.postMessage([{ result: policy.current }], "*")
+      iframe.current?.contentWindow?.postMessage([{ result: policy }], "*")
       return
     }
 
     if (request.method === "csp_set") {
-      policy.current = request.params[0]
-      setKey(crypto.randomUUID())
+      setPolicy(request.params[0])
       return
     }
 
     if (request.method === "html_show") {
-      setShown(true)
+      setHidden(false)
       return
     }
 
     throw new Error()
+  }, [policy])
+
+  const routeOrThrowAsRef = useRef(routeOrThrow)
+  routeOrThrowAsRef.current = routeOrThrow
+
+  const onMessage = useCallback((event: MessageEvent<Explicit>) => {
+    if (event.origin !== location.origin)
+      return
+    routeOrThrowAsRef.current(event)
   }, [])
 
-  const offMessage = useMemo(() => {
+  useEffect(() => {
     addEventListener("message", onMessage)
     return () => removeEventListener("message", onMessage)
   }, [onMessage])
 
-  useEffect(() => () => {
-    offMessage()
-  }, [offMessage])
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    setReady(true)
+  }, [])
+
+  if (!ready)
+    return null
 
   // @ts-ignore
-  return <iframe height={shown ? "auto" : 0} key={key} ref={iframe} csp={policy.current} src={href.join("/")} />
+  return <iframe height={hidden ? "auto" : 0} ref={iframe} csp={policy} src={href} />
 }
