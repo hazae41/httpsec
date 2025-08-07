f282ab269ea5b54379ab81707a753a2e60f53d63
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-08-07 12:33:40 +0700

    d

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,5 +27,8 @@ export default function App({ Component, pageProps }: AppProps) {
   if (!ready)
     return null
 
-  return <Component {...pageProps} />;
+  return <>
+    <div className="static" />
+    <Component {...pageProps} />;
+  </>
 }
