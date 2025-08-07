aefb095e32e5f72f7bc93b2445f5155dff55f7ca
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-08-07 22:49:45 +0700

    d

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,5 @@ export default function App({ Component, pageProps }: AppProps) {
   if (!ready)
     return null
 
-  return <>
-    <Component {...pageProps} />
-  </>
+  return <Component {...pageProps} />
 }
